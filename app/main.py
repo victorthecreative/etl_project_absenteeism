96c4df6216c95e29870e8f46ebbe47bd0bb3d21e
victorthecreative <vitaopoter@hotmail.com>
2024-04-08 20:53:57 -0300

    formatação do código

--- app/main.py
+++ app/main.py
@@ -1,9 +1,8 @@
 from pipe.extract import extract_from_excel
-from pipe.transform import concat_data_frames
 from pipe.load import load_excel
+from pipe.transform import concat_data_frames
 
-
-if __name__ == "__main__":
-  dataframe_list = extract_from_excel("data/input") 
-  data_frame = concat_data_frames(dataframe_list)
-  load_excel = load_excel(data_frame, 'data/output', 'output_file')
+if __name__ == '__main__':
+    dataframe_list = extract_from_excel('data/input')
+    data_frame = concat_data_frames(dataframe_list)
+    load_excel = load_excel(data_frame, 'data/output', 'output_file')
--- app/pipe/transform.py
+++ app/pipe/transform.py
@@ -1,8 +1,11 @@
+from typing import List
+
 import pandas as pd
-from typing import List 
+
 """
 função para transformar uma lista de dataframes em um unico dataframe
 """
 
+
 def concat_data_frames(data_frame_list: List[pd.DataFrame]) -> pd.DataFrame:
-  return pd.concat(data_frame_list)  
+    return pd.concat(data_frame_list)

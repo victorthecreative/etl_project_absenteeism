f633cbb398e37550e9b574b0c57159dd2d839552
victorthecreative <vitaopoter@hotmail.com>
2024-04-16 19:54:08 -0300

    .

--- app/pipe/load.py
+++ app/pipe/load.py
@@ -2,19 +2,18 @@ import os
 
 import pandas as pd
 
-"""
-receber um dataframe e salvar como excel
+def load_excel(data_frame: pd.DataFrame, output_path: str, file_name: str) -> str:
+    """
+    Recebe um dataframe concatenado de todos os outros arquivos e salva em
+    um só arquivo excel.
 
-args:
+    args:
+    data_frame: Conjunto de dados unificados de todos os arquivos
+    output_path: Caminho onde vamos salvar o arquivo
+    file_name: Nome do arquivo Excel a ser gerado    
 
-
-return: "Arquivo salvo com sucesso"
-"""
-
-
-def load_excel(
-    data_frame: pd.DataFrame, output_path: str, file_name: str
-) -> str:
+    return: "Arquivo salvo com sucesso"
+    """
     if not os.path.exists(output_path):
         os.makedirs(output_path)
     data_frame.to_excel(f'{output_path}/{file_name}.xlsx', index=False)
--- app/pipe/transform.py
+++ app/pipe/transform.py
@@ -2,10 +2,17 @@ from typing import List
 
 import pandas as pd
 
-"""
-função para transformar uma lista de dataframes em um unico dataframe
-"""
+
 
 
 def concat_data_frames(data_frame_list: List[pd.DataFrame]) -> pd.DataFrame:
+    """
+    Função que concatena todos os dataframes em um só.
+    
+    args:
+    data_frame_list: Lista com todos os dataframes gerados, de todos os arquivos 
+    Excel.
+
+    return: Dataframe concatenado com todos os arquivos Excel
+    """
     return pd.concat(data_frame_list)

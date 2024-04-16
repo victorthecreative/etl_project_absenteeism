a75a4e37774260b995e40f767f6d2551f6027701
victorthecreative <vitaopoter@hotmail.com>
2024-04-16 19:12:06 -0300

    add prints em cada processo

--- app/main.py
+++ app/main.py
@@ -4,7 +4,9 @@ from pipe.transform import concat_data_frames
 
 if __name__ == '__main__':
     dataframe_list = extract_from_excel('data/input')
+    print("extraido do excel")
     data_frame = concat_data_frames(dataframe_list)
+    print("concatenado em um unico dataframe")
     load_excel = load_excel(data_frame, 'data/output', 'output_file')
-
+    print(" salvo em um excel só")
 
